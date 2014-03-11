b1bd00fa619cb80cf8a6d37026087df7e5aa7cd9
Yanan Liu <Yanan Liu@localhost>
2014-03-11 22:17:16 +0800

    Display all is working ~

--- clc/common/Constants.java
+++ clc/common/Constants.java
@@ -1,6 +1,7 @@
 package clc.common;
 
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 
 import clc.logic.Task;
 
@@ -28,15 +29,29 @@ public class Constants {
 	public static final String MESSAGE_TIMED_TASK_ADDED = "%1$s(%2$s - %3$s) is added.";
 	public static final String MESSAGE_DEADLINE_TASK_ADDED = "%1$s(ending at %2$s) is added.";
 	public static final String MESSAGE_FLOATING_TASK_ADDED = "%1$s is added.";
-	public static final String MESSAGE_TASK_DELETED = "“%1$s” is deleted.";
+	public static final String MESSAGE_TASK_DELETED = "ï¿½%1$sï¿½ is deleted.";
 	public static final String MESSAGE_OUT_OF_BOUND = "%d is out of bound";
-	public static final String MESSAGE_MARK_DONE = "mark “%1$s” as done sucessfuly";
-	public static final String MESSAGE_MARK_NOT_DONE = "mark “%1$s” as not done sucessfuly";
-	public static final String MESSAGE_PREVIOUSLY_MARK_DONE = "“%1$s” is already marked as done";
-	public static final String MESSAGE_PREVIOUSLY_MARK_NOT_DONE = "“%1$s” is originally not mark as done";
+	public static final String MESSAGE_MARK_DONE = "mark ï¿½%1$sï¿½ as done sucessfuly";
+	public static final String MESSAGE_MARK_NOT_DONE = "mark ï¿½%1$sï¿½ as not done sucessfuly";
+	public static final String MESSAGE_PREVIOUSLY_MARK_DONE = "ï¿½%1$sï¿½ is already marked as done";
+	public static final String MESSAGE_PREVIOUSLY_MARK_NOT_DONE = "ï¿½%1$sï¿½ is originally not mark as done";
+	
+	//Display
+	public final static String TO = "to";
+	public static final String MESSAGE_SHOW_ALL_TASKS = "All Tasks";
+	public static final String MESSAGE_OUTPUT_TIMED_TASKS = "%1$d. %2$s, starts at %3$s; ends at %4$s"; 
+	public static final String MESSAGE_OUTPUT_DEADLINE_TASKS = "%1$d. %2$s, ends at %3$s";
+	public static final String MESSAGE_OUTPUT_FLOATING_TASKS = "%d. %s";
+	public static final String MESSAGE_DISPLAY = "All tasks have been displayed.";
+	public static final String MESSAGE_EMPTY_LIST = "Your list is empty";
+	public final static String MESSAEG_DISPLAY_IN_PERIOD = "This period's task(s) has(have) been displayed.";
+	public final static String MESSAGE_NO_TASKS_IN_PERIOD =  "No task is found in this period";
 	
 	public static final String OUTFILE = "data.txt";
 	public static final String HELPFILE = "generalHelp.txt";
 	
 	public static ArrayList<Task> internalMem = new ArrayList<Task>();
+	public static ArrayList<String> DisplayOutput = new ArrayList<String>();
+	public static ArrayList<Task> displayMem = new ArrayList<Task>();
+	public static SimpleDateFormat D_M_Y_DateFormatter = new SimpleDateFormat("EEE, d MMM yyyy h.mm a");
 }
--- clc/logic/Display.java
+++ clc/logic/Display.java
@@ -1,14 +1,124 @@
 package clc.logic;
 
-public class Display implements Command {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+import java.util.zip.DataFormatException;
 
-	public Display() {
-		// TODO Auto-generated constructor stub
+
+import static clc.common.Constants.*;
+
+
+
+public class Display implements Command {
+    private String feedback;
+    private String commandDetails = null;
+    private ArrayList<GregorianCalendar> time;
+    private Calendar startOfPeriod = null, endOfPeriod = null;
+    
+	public Display(ArrayList<GregorianCalendar> time) {
+		this.time = time;
+	}
+	public Display(String commandDetails) {
+		this. commandDetails = commandDetails;
+	} 
+	 
+	@Override
+	public String execute() {
+		
+		DisplayOutput.clear();
+		displayMem.clear();
+		
+		if(internalMem.isEmpty()) {
+			feedback = MESSAGE_EMPTY_LIST;
+		} else if (commandDetails!= null) {// for now i assume all commandDetails are all
+			displayAllTasks();
+		} else {
+			displayInPeriod();
+		}		
+		assert feedback != null;
+		return feedback;
 	}
 	
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
+	private void displayAllTasks(){
+		DisplayOutput.add(MESSAGE_SHOW_ALL_TASKS);
+
+	    for (int i = 1; i<= internalMem.size(); i++){
+	    	
+	    	Task task = internalMem.get(i-1);
+	    	if(task.getTaskType() == TYPE_TIMED_TASK){
+	    		
+	    		String startTime = D_M_Y_DateFormatter.format(task.getStartTime().getTime());
+	    		String endTime = D_M_Y_DateFormatter.format(task.getEndTime().getTime());
+	    		DisplayOutput.add(String.format(MESSAGE_OUTPUT_TIMED_TASKS, i, task.getTaskName(), startTime, endTime));
+	    		
+	    	} else if (task.getTaskType() == TYPE_DEADLINE_TASK){
+	    		
+	    		String endTime = D_M_Y_DateFormatter.format(task.getEndTime().getTime());
+	    		DisplayOutput.add(String.format(MESSAGE_OUTPUT_DEADLINE_TASKS, i, task.getTaskName(), endTime));
+	    	} else {
+	    		DisplayOutput.add(String.format(MESSAGE_OUTPUT_FLOATING_TASKS, i, task.getTaskName()));
+	    	}
+	    	displayMem.add(task);
+	    }
+	 
+	    printOutDisplay();
+	    feedback = MESSAGE_DISPLAY;
 	}
+	
+	private void displayInPeriod(){
+			startOfPeriod = time.get(0);
+			endOfPeriod = time.get(1);
+			//Process output
+			DisplayOutput.add(D_M_Y_DateFormatter.format(startOfPeriod.getTime()) + TO + D_M_Y_DateFormatter.format(endOfPeriod.getTime()));
+			
+			goThroughTimePeriod(startOfPeriod, endOfPeriod);
+			printOutDisplay();
+			
+			// Process feedback
+			if (!isDataEmpty()) {
+				feedback = MESSAEG_DISPLAY_IN_PERIOD;
+			} else {
+				feedback = MESSAGE_NO_TASKS_IN_PERIOD;
+			}
+	
+	}
+	
+	// Go through the internal memory and check whether the tasks are with in the time period
+		private void goThroughTimePeriod(Calendar startOfPeriod, Calendar endOfPeriod) {
+			int taskNo = 1;
+			for (int i = 1; i <= internalMem.size(); i++) {
+				Calendar taskStartTime = internalMem.get(i-1).getStartTime();
+				Calendar taskEndTime = internalMem.get(i-1).getEndTime();
+				if (isWithinTimePeriod(startOfPeriod, endOfPeriod, taskStartTime, taskEndTime)) {
+					DisplayOutput.add(String.format(MESSAGE_OUTPUT_FLOATING_TASKS, taskNo, internalMem.get(i-1).getTaskName()));
+					displayMem.add(internalMem.get(i-1));
+					taskNo++;
+				}	
+			}
+		}
+
+		/**
+		 * 
+		 * Firstly, it checks through the start time
+		 * Secondly, it checks through the end time
+		 * Lastly, it checks if the date 
+		 * If either one of the condition is true, the date is within the taskStartDate and taskEndDate
+		 */
+		private boolean isWithinTimePeriod(Calendar startOfPeriod, Calendar endOfPeriod, Calendar taskStartTime, Calendar taskEndTime) {
+			return true;
+		}
+		
+		// Check whether the data which can be processed is empty
+		protected boolean isDataEmpty() {
+			return displayMem.isEmpty();
+		}
+		
+		private void printOutDisplay(){
+			for (int i = 0; i < DisplayOutput.size(); i++) {
+				System.out.println(DisplayOutput.get(i));
+			}
+		}
+
 }
