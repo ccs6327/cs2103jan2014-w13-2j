8eb1c6367e4ab82b3ee693105f3bc89156579654
ccs6327 <ccs6327@gmail.com>
2014-03-15 03:27:43 +0800

    clear DisplayMem after delete and clear

--- clc/logic/Clear.java
+++ clc/logic/Clear.java
@@ -1,12 +1,16 @@
 package clc.logic;
 
 import static clc.common.Constants.*;
+
+import java.util.ArrayList;
+
 import clc.storage.Storage;
 
 public class Clear implements Command {
 	@Override
 	public String execute() {
 		internalMem.clear();
+		displayMem = new ArrayList<Task>();
 		addNewVersion();
 		Storage.writeContentIntoFile();
 		return MESSAGE_CLEARED;
