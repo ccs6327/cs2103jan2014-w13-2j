441b888f6b68d40e855e9ddac04315e3c6cb48b1
ccs6327 <ccs6327@gmail.com>
2014-03-17 23:11:09 +0800

    commit InvalidInputException and throws exception in SequenceNumberExtractor

---
+++ clc/common/InvalidInputException.java
@@ -0,0 +1,14 @@
+package clc.common;
+
+public class InvalidInputException extends Exception {
+
+	public InvalidInputException() {
+		super();
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
