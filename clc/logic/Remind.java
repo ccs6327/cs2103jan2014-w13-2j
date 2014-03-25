f9b7b006868a1744a9a0d98e4ca1b0b6294e0961
ccs6327 <ccs6327@gmail.com>
2014-03-25 14:37:02 +0800

    refactored Reminder.java

--- clc/logic/Remind.java
+++ clc/logic/Remind.java
@@ -10,7 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 import clc.storage.Storage;
 
