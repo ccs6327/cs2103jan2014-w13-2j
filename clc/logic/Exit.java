7aa76c3bfae7d88b1432987198d74553d02e9c95
mikecc2008 <mikecc2008@localhost>
2014-03-28 03:05:08 +0800

    Minor change in Exit: write internalMem into file before exit to ensure the data file is updated

--- clc/logic/Exit.java
+++ clc/logic/Exit.java
@@ -1,9 +1,12 @@
 package clc.logic;
 
+import clc.storage.Storage;
+
 public class Exit implements Command {
 
 	@Override
 	public String execute() {
+		Storage.writeContentIntoFile();
 		System.exit(0);
 		return null;
 	}
