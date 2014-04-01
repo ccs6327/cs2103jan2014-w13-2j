a6608e28d1d1b29d12f1f0ecd9f2e86e793e745e
mikecc2008 <mikecc2008@localhost>
2014-04-02 04:44:50 +0800

    V0.3

---
+++ clc/common/InvalidInputException.java
@@ -0,0 +1,14 @@
+package clc.common;
+
+public class InvalidInputException extends Exception {
+
+	public InvalidInputException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ clc/ui/SequenceNumberExtractor.java
@@ -0,0 +1,52 @@
+package clc.ui;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import clc.common.InvalidInputException;
+import static clc.common.Constants.ERROR_CONTAIN_NON_NUMERIC_INFO;
+import static clc.common.Constants.ERROR_EMPTY_COMMAND_DETAILS;
+
+public class SequenceNumberExtractor extends Analyzer{
+	
+
+	protected SequenceNumberExtractor(String input) {
+		super(input);
+	}
+	
+	protected static ArrayList<Integer> getSequenceNum() throws InvalidInputException {
+		return parseDetailsToSequenceNum();
+	}
+
+	private static ArrayList<Integer> parseDetailsToSequenceNum() throws InvalidInputException {
+		ArrayList<Integer> taskSeqNo = new ArrayList<Integer>();
+		
+		determineIfCommandDetailsExist();
+		
+		Scanner sc = new Scanner(commandDetails);
+		while (sc.hasNext()) {
+			String currWord = sc.next();
+			if (isNumeric(currWord)) {
+				if (!taskSeqNo.contains(Integer.parseInt(currWord))) { //avoid duplicate
+					taskSeqNo.add(Integer.parseInt(currWord));
+				}
+			} else {
+				sc.close();
+				throw new InvalidInputException(ERROR_CONTAIN_NON_NUMERIC_INFO);
+			}
+		}
+		sc.close();
+		return new ArrayList<Integer>(taskSeqNo);
+	}
+
+	private static void determineIfCommandDetailsExist()
+			throws InvalidInputException {
+		if (!doesCommandDetailsExist()) {
+			throw new InvalidInputException(ERROR_EMPTY_COMMAND_DETAILS);
+		}
+	}
+
+	private static boolean doesCommandDetailsExist() {
+		return !commandDetails.equals("");
+	}
+}
