127f0bd627f940c710ac23e1be3d3a5fcd0409bf
ccs6327 <ccs6327@gmail.com>
2014-04-02 15:41:36 +0800

    override processCalendarInfo in UpdateAnalyzer to be less restricted

---
+++ clc/ui/TestingUpdateAnalyzer.java
@@ -0,0 +1,185 @@
+package clc.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import clc.common.InvalidInputException;
+
+import static clc.common.Constants.ERROR_NO_COMMA;
+import static clc.common.Constants.ERROR_NO_SEQUENCE_NUMBER;
+import static clc.common.Constants.ERROR_NO_NEW_TASK_NAME;
+import static clc.common.Constants.ERROR_EMPTY_COMMAND_DETAILS;
+
+public class TestingUpdateAnalyzer {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCalendarProvided() throws InvalidInputException {
+		/*
+		 * D for Date
+		 * T for Time
+		 */
+		
+		//case update , T
+		Analyzer.analyze("update 1 , 1159pm");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(1, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update , D
+		Analyzer.analyze("update 1 , 1/1/2100");
+
+		UpdateAnalyzer.analyze();
+		assertEquals(2, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update , D T
+		Analyzer.analyze("update 1 , 1/1/2100 1159pm");
+
+		UpdateAnalyzer.analyze();
+		assertEquals(3, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update T ,
+		Analyzer.analyze("update 1 1159pm ,");
+
+		UpdateAnalyzer.analyze();
+		assertEquals(4, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update T , T
+		Analyzer.analyze("update 1 1158pm , 1159pm");
+
+		UpdateAnalyzer.analyze();
+		assertEquals(5, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update T , D
+		Analyzer.analyze("update 1 1159pm , 1/1/2100");
+
+		UpdateAnalyzer.analyze();
+		assertEquals(6, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update T , D T
+		Analyzer.analyze("update 1 1159pm , 1/1/2100 1159pm");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(7, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D ,
+		Analyzer.analyze("update 1 1/1/2100 ,");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(8, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D , T
+		Analyzer.analyze("update 1 1/1/2100 , 1159pm");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(9, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D , D
+		Analyzer.analyze("update 1 1/1/2100 , 2/1/2100");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(10, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D , D T
+		Analyzer.analyze("update 1 1/1/2100 , 2/1/2100 1159pm");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(11, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D T ,
+		Analyzer.analyze("update 1 1/1/2100 1159pm ,");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(12, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D T , T
+		Analyzer.analyze("update 1 1/1/2100 1159pm , 1159pm");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(13, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D T , D
+		Analyzer.analyze("update 1 1/1/2100 1159pm , 2/1/2100");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(14, UpdateAnalyzer.getCalendarProvidedCase());
+		
+		//case update D T , D T
+		Analyzer.analyze("update 1 1/1/2100 1159pm , 2/1/2100 1159pm");
+		
+		UpdateAnalyzer.analyze();
+		assertEquals(15, UpdateAnalyzer.getCalendarProvidedCase());
+	}
+	
+	@Test
+	public void test() {
+		//no details
+		Analyzer.analyze("update");
+		
+		try {
+			UpdateAnalyzer.analyze();
+		} catch (InvalidInputException e) {
+			assertEquals(ERROR_EMPTY_COMMAND_DETAILS, e.getMessage());
+		}
+		
+		
+		//spaces details
+		Analyzer.analyze("update           ");
+		
+		try {
+			UpdateAnalyzer.analyze();
+		} catch (InvalidInputException e) {
+			assertEquals(ERROR_EMPTY_COMMAND_DETAILS, e.getMessage());
+		}
+		
+		
+		//no sequence number
+		//case1 update task name
+		Analyzer.analyze("update taskname");
+
+		try {
+			UpdateAnalyzer.analyze();
+		} catch (InvalidInputException e) {
+			assertEquals(ERROR_NO_SEQUENCE_NUMBER, e.getMessage());
+		}
+		
+		//case 2 update calendar
+		Analyzer.analyze("update 12/12/2112 4pm ,");
+
+		try {
+			UpdateAnalyzer.analyze();
+		} catch (InvalidInputException e) {
+			assertEquals(ERROR_NO_SEQUENCE_NUMBER, e.getMessage());
+		}
+		
+		
+		//no task name **CALENDAR CASE IS CONSIDERED BY doesContainTimeInfo() ALREADY
+		Analyzer.analyze("update 1");
+
+		try {
+			UpdateAnalyzer.analyze();
+		} catch (InvalidInputException e) {
+			assertEquals(ERROR_NO_NEW_TASK_NAME, e.getMessage());
+		}
+		
+		
+		//no comma in calendar details
+		Analyzer.analyze("update 1 4pm 5pm");
+
+		try {
+			UpdateAnalyzer.analyze();
+		} catch (InvalidInputException e) {
+			assertEquals(ERROR_NO_COMMA, e.getMessage());
+		}
+	}
+}
---
+++ clc/ui/UpdateAnalyzer.java
@@ -0,0 +1,194 @@
+package clc.ui;
+
+import static clc.common.Constants.SPACE;
+import static clc.common.Constants.COMMA;
+import static clc.common.Constants.ERROR_NO_COMMA;
+import static clc.common.Constants.ERROR_NO_SEQUENCE_NUMBER;
+import static clc.common.Constants.ERROR_NO_NEW_TASK_NAME;
+import static clc.common.Constants.ERROR_EMPTY_COMMAND_DETAILS;
+
+import java.text.ParseException;
+import java.util.GregorianCalendar;
+
+import clc.common.InvalidInputException;
+
+public class UpdateAnalyzer extends TimeParser {
+	private static boolean isCaseUpdateCalendar;
+	private static String[] tempInfo;
+	private static int calendarProvided = 0;
+	private static int seqNo;
+	private static GregorianCalendar tempStartCalendar;
+
+	protected UpdateAnalyzer(String input) {
+		super(input);
+	}
+
+	protected static void analyze() throws InvalidInputException {
+		determineIfCommandDetailsEmpty();
+		
+		calendarProvided = 0;
+		tempInfo = commandDetails.split(SPACE);
+		infoToBeProcessed = tempInfo;
+		
+		determineIfSeqNoProvided();
+		processCalendarInfo();
+
+		if (doesContainTimeInfo()) { //case update calendar
+			if (!commandDetails.contains(COMMA)) {
+				throw new InvalidInputException(ERROR_NO_COMMA);
+			}
+
+			int indexOfComma = findIndexOfComma();
+			analyzeUpdateStartTime(indexOfComma);
+			
+			analyzeUpdateEndTime(indexOfComma);
+			startCalendar = tempStartCalendar;
+
+			isCaseUpdateCalendar = true;
+		} else { //case update task name
+			isCaseUpdateCalendar = false;
+		}
+	}
+
+	private static void determineIfCommandDetailsEmpty()
+			throws InvalidInputException {
+		if (commandDetails.equals("")) {
+			throw new InvalidInputException(ERROR_EMPTY_COMMAND_DETAILS);
+		}
+	}
+
+	private static void determineIfSeqNoProvided() throws InvalidInputException {
+		if (!isNumeric(getFirstWord(commandDetails))) {
+			throw new InvalidInputException(ERROR_NO_SEQUENCE_NUMBER);
+		} else {
+			seqNo = Integer.parseInt(getFirstWord(commandDetails));
+		}
+	}
+
+	private static void analyzeUpdateStartTime(int indexOfComma) throws InvalidInputException {
+		int index = 0;
+		if (indexOfComma > 0) {
+			infoToBeProcessed = new String[indexOfComma - 1];
+			for (int i = 1; i < indexOfComma; i ++) { //first one is sequence no
+				infoToBeProcessed[index ++] = tempInfo[i];
+			}
+			processCalendarInfo();
+
+			// as processCalendarInfo will set the time to endTime
+			// so we have to swap the value
+			tempStartCalendar = endCalendar;
+			endCalendar = null;
+
+			determineIfStartDateIsProvided();
+			determineIfStartTimeIsProvided();
+		}
+	}
+
+	private static void analyzeUpdateEndTime(int indexOfComma) throws InvalidInputException {
+		int index = 0;
+		if (tempInfo.length != indexOfComma + 1) {
+			infoToBeProcessed = new String[tempInfo.length - indexOfComma - 1];
+			for (int i = indexOfComma + 1; i < tempInfo.length ; i ++) { //first one is sequence no
+				infoToBeProcessed[index ++] = tempInfo[i];
+			}
+	
+			processCalendarInfo();
+
+			determineIfEndDateIsProvided();
+			determineIfEndTimeIsProvided();
+		}
+	}
+
+	private static void determineIfStartDateIsProvided() {
+		if (isEndDateSet) {
+			calendarProvided += 8;
+		}
+	}
+
+	private static void determineIfStartTimeIsProvided() {
+		if (isEndTimeSet) {
+			calendarProvided += 4;
+		}
+	}
+
+	private static void determineIfEndDateIsProvided() {
+		if (isEndDateSet) {
+			calendarProvided += 2;
+		}
+	}
+
+	private static void determineIfEndTimeIsProvided() {
+		if (isEndTimeSet) {
+			calendarProvided += 1;
+		}
+	}
+
+	private static int findIndexOfComma() {
+		for(int i = 0; i < tempInfo.length; i ++) {
+			if(tempInfo[i].equals(COMMA)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static boolean getUpdateCase() {
+		return isCaseUpdateCalendar;
+	}
+
+	protected static int getSeqNumForUpdate() throws InvalidInputException {
+		return seqNo;
+	}
+
+	protected static String getNewTaskName() throws InvalidInputException {
+		String newTaskName = removeFirstWord(commandDetails);
+		if (isNewTaskNameProvided(newTaskName)) {
+			throw new InvalidInputException(ERROR_NO_NEW_TASK_NAME);
+		}
+		return newTaskName;
+	}
+
+	private static boolean isNewTaskNameProvided(String newTaskName) {
+		return newTaskName.equals("");
+	}
+
+	public static int getCalendarProvidedCase() {
+		return calendarProvided;
+	}
+	
+	//override function in TimeParser
+	protected static void processCalendarInfo() throws InvalidInputException {
+		initializeVariable();
+		int currIndex = infoToBeProcessed.length - 1;
+		
+		while (currIndex >= 0 && !hasAllTimeSet()) {
+			String toBeAnalyzedString = "";
+			int loopIndex = currIndex;
+			for (int i = 0; i < 3; i ++) { // calendar at most represent by 3 Strings
+				toBeAnalyzedString = infoToBeProcessed[loopIndex] +  toBeAnalyzedString;
+				try {
+					System.out.println(toBeAnalyzedString);
+					analyzeTime(toBeAnalyzedString);
+					if (endCalendarIndex == -1) {
+						startCalendarIndex = loopIndex;
+						endCalendarIndex = loopIndex;
+					} else if (startCalendarIndex == -1 || loopIndex < startCalendarIndex) {
+						startCalendarIndex = loopIndex;
+					}
+					currIndex = loopIndex;
+					break;
+				} catch (ParseException e) { //catch parsing error
+					if (loopIndex - 1 < 0) {
+						break;
+					}
+				}
+				loopIndex --;
+			}
+			currIndex --;
+			setCalendar();
+		}
+		
+		setStartCalendarAsNullIfNotSet();
+		setEndCalendarAsNullIfNotSet();
+	}
+}
