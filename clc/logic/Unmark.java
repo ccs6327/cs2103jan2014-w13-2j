df0cb6513e3a6a4e14a31818098873c188fdd71a
ccs6327 <ccs6327@gmail.com>
2014-04-11 01:20:51 +0800

    refactor Unmark.java

--- clc/logic/Unmark.java
+++ clc/logic/Unmark.java
@@ -1,10 +1,16 @@
+//author A0112089J
+
 package clc.logic;
 
 import java.util.ArrayList;
 
 import clc.storage.History;
 import clc.storage.Storage;
-import static clc.common.Constants.*;
+
+import static clc.common.Constants.NEWLINE;
+import static clc.common.Constants.MESSAGE_OUT_OF_BOUND;
+import static clc.common.Constants.MESSAGE_MARK_NOT_DONE;
+import static clc.common.Constants.MESSAGE_PREVIOUSLY_MARK_NOT_DONE;
 
 public class Unmark implements Command {
 	private ArrayList<Integer> taskSeqNo;
@@ -20,37 +26,64 @@ public class Unmark implements Command {
 
 	@Override
 	public String execute() {
+		boolean isChanged = unmarkTasks();
+		updateHistoryAndDatabaseIfChanged(isChanged);
+		return feedback.toString().trim();
+	}
+
+	private boolean unmarkTasks() {
 		boolean isChanged = false;
-		
 		for (int i = 0; i < taskSeqNo.size(); i++) {
 			int seqNo = taskSeqNo.get(i);
-
-			if (isOutOfBound(displayMem.size(), seqNo)) {
-				feedback.append(String.format(MESSAGE_OUT_OF_BOUND, seqNo));
-				feedback.append("\n");
-			} else {
-				boolean isUnmarked = false;
-				int internalSeqNo = displayMem.get(seqNo - 1);
-				isUnmarked = internalMem.get(internalSeqNo).markUndone();
-				String taskName = internalMem.get(internalSeqNo).getTaskName();
-				
-				if (isUnmarked) {
-					isChanged = true;
-					feedback.append(String.format(MESSAGE_MARK_NOT_DONE, taskName));
-					feedback.append("\n");
-				} else { //task is not marked as done previously
-					feedback.append(String.format(MESSAGE_PREVIOUSLY_MARK_NOT_DONE, taskName));
-					feedback.append("\n");
-				}
-			}
+			isChanged = markTaskIfInBoundary(isChanged, seqNo);
 		}
+		return isChanged;
+	}
+
+	private boolean markTaskIfInBoundary(boolean isChanged, int seqNo) {
+		if (isOutOfBound(displayMem.size(), seqNo)) {
+			appendOutOfBoundMessage(seqNo);
+		} else {
+			isChanged = markTaskAndAppendFeedbackMessage(isChanged, seqNo);
+		}
+		return isChanged;
+	}
+
+	private void appendOutOfBoundMessage(int seqNo) {
+		feedback.append(String.format(MESSAGE_OUT_OF_BOUND, seqNo));
+		feedback.append(NEWLINE);
+	}
+
+	private boolean markTaskAndAppendFeedbackMessage(boolean isChanged, int seqNo) {
+		boolean isUnmarked = false;
+		int internalSeqNo = displayMem.get(seqNo - 1);
+		isUnmarked = internalMem.get(internalSeqNo).markUndone();
+		String taskName = internalMem.get(internalSeqNo).getTaskName();
 		
+		if (isUnmarked) {
+			isChanged = true;
+			appendMarkNotDoneMessage(taskName);
+		} else { //task is not marked as done previously
+			appendPreviouslyNotMarkDoneMessage(taskName);
+		}
+		return isChanged;
+	}
+
+	private void appendMarkNotDoneMessage(String taskName) {
+		feedback.append(String.format(MESSAGE_MARK_NOT_DONE, taskName));
+		feedback.append(NEWLINE);
+	}
+
+	private void appendPreviouslyNotMarkDoneMessage(String taskName) {
+		feedback.append(String.format(MESSAGE_PREVIOUSLY_MARK_NOT_DONE, taskName));
+		feedback.append(NEWLINE);
+	}
+
+	private void updateHistoryAndDatabaseIfChanged(boolean isChanged) {
 		if (isChanged) {
 			History.addNewVersion();
 			Storage.writeContentIntoFile();
 		}
-		
-		return feedback.toString().trim();
 	}
 
 	private boolean isOutOfBound(int taskListSize, int seqNo) {
