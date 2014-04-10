ef860b122c1c60460777079cc2261730c7e125c6
ccs6327 <ccs6327@gmail.com>
2014-04-10 23:37:47 +0800

    resolve regression after catching PatternSyntaxException

--- clc/ui/Analyzer.java
+++ clc/ui/Analyzer.java
@@ -3,6 +3,7 @@
 package clc.ui;
 
 import static clc.common.Constants.EMPTY;
+import static clc.common.Constants.SPACE;
 import static clc.common.Constants.ERROR_EMPTY_COMMAND_DETAILS;
 import static clc.common.Constants.MESSAGE_INVALID_FORMAT;
 
@@ -30,14 +31,14 @@ public class Analyzer {
 	}
 	
 	protected static String getFirstWord(String input) {
-		String commandType = input.trim().split("\\s+")[0];
+		String commandType = input.trim().split(SPACE)[0];
 		return commandType;
 	}
 
 	protected static String removeFirstWord(String input) throws InvalidInputException {
 		String firstWord;
 		try {
-			firstWord = input.replaceFirst("\\" + getFirstWord(input), EMPTY).trim();
+			firstWord = input.replaceFirst(getFirstWord(input), EMPTY).trim();
 		} catch (PatternSyntaxException e) {
 			throw new InvalidInputException(String.format(MESSAGE_INVALID_FORMAT, input));
 		}
