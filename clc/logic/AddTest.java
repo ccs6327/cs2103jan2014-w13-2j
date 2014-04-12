dc85b7a3bccd85d0a2b62a276d61976157304674
mikecc2008 <mikecc2008@localhost>
2014-04-12 17:20:59 +0800

    Added: test cases for Add

---
+++ clc/logic/AddTest.java
@@ -0,0 +1,63 @@
+/**
+ * Command Line Calendar (CLC)
+ * AddTest.java
+ * 
+ * This is JUnit test cases for Add.
+ */
+//@author A0105712U
+package clc.logic;
+
+import static clc.common.Constants.*;
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
+import clc.common.LogHelper;
+
+public class AddTest {
+
+	private static final long OFFSET = 600000;
+
+	@Test
+	public void test() {
+		
+		Calendar startTime = new GregorianCalendar();
+		startTime.setTimeInMillis(System.currentTimeMillis() + OFFSET);
+		Calendar endTime = new GregorianCalendar();
+		endTime.setTimeInMillis(System.currentTimeMillis() + OFFSET);
+		
+		// Test case 1: add a timed task	
+		Task timedTask = new Task(TIMED_TASK, startTime, endTime);
+		Add add1 = new Add(timedTask);
+		assertEquals(String.format(MESSAGE_TIMED_TASK_ADDED, timedTask.getTaskName(), formatDate(timedTask.getStartTime()), formatDate(timedTask.getEndTime())), add1.execute());
+		LogHelper.info("Test case 1 passed");
+		
+		// Test case 2: add a deadline task	
+		Task deadlineTask = new Task(DEADLINE_TASK, endTime);
+		Add add2 = new Add(deadlineTask);
+		assertEquals(String.format(MESSAGE_DEADLINE_TASK_ADDED, deadlineTask.getTaskName(), formatDate(deadlineTask.getEndTime())), add2.execute());
+		LogHelper.info("Test case 2 passed");
+		
+		// Test case 3: add a floating task	
+		Task floatingTask = new Task(FLOATING_TASK);
+		Add add3 = new Add(floatingTask);
+		assertEquals(String.format(MESSAGE_FLOATING_TASK_ADDED, floatingTask.getTaskName()), add3.execute());
+		LogHelper.info("Test case 3 passed");
+		
+		LogHelper.info("All test cases passed\r\n");
+		
+	}
+
+	private String formatDate(Calendar calendar) {
+		String date;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy h.mm a");
+		date = dateFormat.format(calendar.getTime());
+		date = date.replaceAll("AM", "am");
+		date = date.replaceAll("PM", "pm");
+		return date;
+	}
+}
